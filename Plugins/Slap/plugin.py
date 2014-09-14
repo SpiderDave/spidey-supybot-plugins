eb6440d85c3e44964d4628e5a76e982c6a9f725e
SpiderDave <ifallelsefailsusefire@gmail.com>
2014-09-13 20:17:19 -0400

    Slap: added some changes thanks to FUBAR.  Note: I kept the binary file mode ("wb" vs "w"), because I don't want it writing different things to Windows vs *nix (even if it looks wrong in notepad).

--- Plugins/Slap/plugin.py
+++ Plugins/Slap/plugin.py
@@ -28,24 +28,24 @@ class _Plugin(callbacks.Plugin):
         slapFile='%s%sslap.txt' % (conf.supybot.directories.data(), os.sep)
         if not os.path.isfile(slapFile):
             try:
-                f = open(slapFile, 'w+b')
+                f = open(slapFile, 'wb')
                 f.write("a large trout" + "\n")
                 f.close()
             except:
                 irc.reply('Error: could not create slap.txt file in the data folder.')
                 return
-        
-        f = open('%s%sslap.txt' % (conf.supybot.directories.data(), os.sep), 'a+b')
+        f = open(slapFile, 'rb')
         l=f.readlines()
-        
+        f.close()
         t=random.choice(l)
         t = t.replace("\'", "'").replace("\r", "").replace("\n", "")
         if not stuff:
             stuff="someone"
         if (stuff.lower() == irc.nick.lower()) or (stuff.lower() == 'yourself'):
             try:
-                fmeta=open('%s%sbot_meta.txt' % (conf.supybot.directories.data(),os.sep) , 'a+b')
+                fmeta=open('%s%sbot_meta.txt' % (conf.supybot.directories.data(),os.sep) , 'rb')
                 gender=fmeta.read().split('gender=')[1].splitlines()[0].strip().lower()
+                fmeta.close()
                 if gender=='male':
                     stuff="himself"
                 elif gender=='female':
@@ -57,16 +57,25 @@ class _Plugin(callbacks.Plugin):
         if stuff.lower() == 'me':
             stuff=msg.nick
         if stuff.split(' ',1)[0].lower()=="--add" or stuff.split(' ',1)[0].lower()=="add":
-            stuff=stuff.split(' ',1)[1]
-            f.write(stuff + "\n")
-            irc.reply(format("puts %s away for a special occasion.  (#%s)",stuff, str(len(l)+1) ), action=True)
+            try:
+                 stuff=stuff.split(' ',1)[1]
+                 f = open(slapFile, 'ab')
+                 f.write(stuff + "\n")
+                 f.close()
+                 irc.reply(format("puts %s away for a special occasion.  (#%s)",stuff, str(len(l)+1) ), action=True)
+            except:
+                 irc.reply("Error: add failed.")
+                 return   
         elif stuff.split(' ',1)[0].lower()=="--plain":
             t = utils.str.normalizeWhitespace(t)
             irc.reply(t)
         elif stuff.split(' ',1)[0].lower()=="--plain2":
-            if stuff.split(' ',1)[1]:
+            try:
+                stuff.split(' ',1)[1]
                 stuff=stuff.split(' ',1)[1]
                 irc.reply(format("slaps %s",stuff) +".", action=True)
+            except:
+                irc.reply("slaps the air") 
         elif stuff.split(' ',1)[0].lower()=="list" or stuff.split(' ',1)[0].lower()=="--list":
             irc.reply("Yo dawg.  I herd you like slaplists, so we put a slap in your list so you get slapped while you list.", prefixNick=False)
             irc.reply(format("slaps %s around a bit with SpiderDave's slaplist",msg.nick) +".", action=True)
@@ -74,13 +83,18 @@ class _Plugin(callbacks.Plugin):
             if not ircdb.checkCapability(msg.prefix,'owner'):
                 irc.error("Only my owner can make me do that.  It's a painful process you know!")
                 return
-            stuff=stuff.split(' ',1)[1]
-            stuff=stuff.strip().lower()
+            try:
+                stuff=stuff.split(' ',1)[1]
+                stuff=stuff.strip().lower()
+            except:
+                irc.error('Error: no gender provided.  valid genders are ' + '/'.join(self.validgenders) + '.')
+                return
             if stuff not in self.validgenders:
                 irc.error('Error: valid genders are ' + '/'.join(self.validgenders) + '.')
                 return
-            fmeta=open('%s%sbot_meta.txt' % (conf.supybot.directories.data(),os.sep) , 'w+b') #wipes file
+            fmeta=open('%s%sbot_meta.txt' % (conf.supybot.directories.data(),os.sep) , 'wb') #wipes file
             fmeta.write('gender=' + stuff + "\n")
+            fmeta.close()
             irc.reply("Gender set to %s" % stuff)
         else:
             irc.reply(format("slaps %s around a bit with ",stuff) + t +".", action=True)
@@ -95,23 +109,23 @@ class _Plugin(callbacks.Plugin):
         attackFile='%s%sattack.txt' % (conf.supybot.directories.data(), os.sep)
         if not os.path.isfile(attackFile):
             try:
-                f = open(attackFile, 'w+b')
+                f = open(attackFile, 'wb')
                 f.write("hits _ in the shins with an oversized ruler" + "\n")
                 f.close()
             except:
-                irc.reply('Error: could not create slap.txt file in the data folder.')
+                irc.reply('Error: could not create attack.txt file in the data folder.')
                 return
         
-        f = open('%s%sattack.txt' % (conf.supybot.directories.data(), os.sep), 'a+b')
+        f = open(attackFile, 'rb')
         l=f.readlines()
-
+        f.close()
         t=random.choice(l)
         t = t.replace("\'", "'").replace("\r", "").replace("\n", "")
         if not stuff:
             stuff="someone"
         if (stuff.lower() == irc.nick.lower()) or (stuff.lower() == 'yourself'):
             try:
-                fmeta=open('%s%sbot_meta.txt' % (conf.supybot.directories.data(),os.sep) , 'a+b')
+                fmeta=open('%s%sbot_meta.txt' % (conf.supybot.directories.data(),os.sep) , 'rb')
                 gender=fmeta.read().split('gender=')[1].splitlines()[0].strip().lower()
                 if gender=='male':
                     stuff="himself"
@@ -122,36 +136,47 @@ class _Plugin(callbacks.Plugin):
             except:
                 stuff="itself"
         if stuff.lower() == 'me':
-            stuff=msgs.nick
+            stuff=msg.nick
         if stuff.split(' ',1)[0].lower()=="--add" or stuff.split(' ',1)[0].lower()=="add":
-            stuff=stuff.split(' ',1)[1]
-            t=stuff
-            if '_' not in stuff:
-                irc.reply("You must have a _ to use as a target ex: attack add punches _ in the solar plexus")
-                return
-            if t.endswith('.'):
-                t=t[:-1]
-            f.write(t + "\n")
-            irc.reply(format("Attack #%s added.",str(len(l)+1) ))
+            try:
+                 stuff=stuff.split(' ',1)[1]
+                 t=stuff
+                 if '_' not in stuff:
+                     irc.reply("You must have a _ to use as a target ex: attack add punches _ in the solar plexus")
+                     return
+                 if t.endswith('.'):
+                    t=t[:-1]
+                 f = open(attackFile, "ab")
+                 f.write(t + "\n")
+                 f.close()
+                 irc.reply(format("Attack #%s added.",str(len(l)+1) ))
+            except:
+                 irc.reply("error:  add failed")
+                 return
         elif stuff.split(' ',1)[0].lower()=="--plain":
-            t = utils.str.normalizeWhitespace(t).replace('_',stuff)
+            t = utils.str.normalizeWhitespace(t).replace('_',"someone")
             t=t.replace("s's","s'")
             
             irc.reply(t)
-        elif stuff.split(' ',1)[0].lower()=="list" or stuff.split(' ',1)[0].lower()=="--list":
+        elif stuff.split(' ',1)[0].lower()=="--list":
             irc.reply("Allow me to demonstrate:", prefixNick=False)
-            irc.reply(t.replace('_',msgs.nick) + "... Twice.", action=True)
+            irc.reply(t.replace('_',msg.nick) + "... Twice.", action=True)
         elif stuff.split(' ',1)[0].lower()=="--setgender":
             if not ircdb.checkCapability(msg.prefix,'owner'):
                 irc.error("Only my owner can make me do that.  It's a painful process you know!")
                 return
-            stuff=stuff.split(' ',1)[1]
-            stuff=stuff.strip().lower()
+            try:
+                stuff=stuff.split(' ',1)[1]
+                stuff=stuff.strip().lower()
+            except:
+                irc.error('Error: no gender provided.  valid genders are ' + '/'.join(self.validgenders) + '.')
+                return
             if stuff not in self.validgenders:
                 irc.error('Error: valid genders are ' + '/'.join(self.validgenders) + '.')
                 return
-            fmeta=open('%s%sbot_meta.txt' % (conf.supybot.directories.data(),os.sep) , 'w+b') #wipes file
+            fmeta=open('%s%sbot_meta.txt' % (conf.supybot.directories.data(),os.sep) , 'wb') #wipes file
             fmeta.write('gender=' + stuff + "\n")
+            fmeta.close()
             irc.reply("Gender set to %s" % stuff)
         else:
             t=t.replace('_',stuff) + "."
