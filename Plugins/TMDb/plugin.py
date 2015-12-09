1313f19d32e536050c8dd1a0a95f31b0910e5906
SpiderDave <ifallelsefailsusefire@gmail.com>
2015-12-09 09:18:38 -0500

    TMDb plugin: removed tmdb3 folder (it wasn't used), added support for languages.

--- Plugins/TMDb/plugin.py
+++ Plugins/TMDb/plugin.py
@@ -10,16 +10,28 @@ import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 import os
 import json
-
+import urllib2
+try:
+    from supybot.i18n import PluginInternationalization, internationalizeDocstring
+except:
+    PluginInternationalization, internationalizeDocstring = lambda x:x, lambda x:x
 
 # This will be used to change the name of the class to the folder name
 PluginName=os.path.dirname( __file__ ).split(os.sep)[-1]
+
+_ = PluginInternationalization(PluginName)
+@internationalizeDocstring
 class _Plugin(callbacks.Plugin):
-    """Add the help for "@plugin help <Plugin Name>" here
-    This should describe *how* to use this plugin."""
+    """Contains commands for checking movie and tv information from themoviedb.org.
+    """
     threaded = True
 
-    def movie(self, irc, msg, args, text):
+    def _get_redirected_url(self, url):
+        opener = urllib2.build_opener(urllib2.HTTPRedirectHandler)
+        request = opener.open(url)
+        return request.url
+
+    def movie(self, irc, msg, args, opts, text):
         """<film>
 
         Get information on <film> from TMDb
@@ -27,20 +39,29 @@ class _Plugin(callbacks.Plugin):
         
         try:
             f = open('%s%stmdbkey.dat' % (conf.supybot.directories.data(), os.sep), 'r')
-            key=f.read().strip()
+            apiKey=f.read().strip()
             f.close
         except:
             irc.reply('Error: missing or key.  Check tmdbkey.dat file in your data folder.')
             return
+
+        # input options
+        language=None
+        for (k, value) in opts:
+            if k == 'language':
+                language=value
+
         base_url = "https://api.themoviedb.org/3/search/movie"
         base_url2 = "https://api.themoviedb.org/3/movie/"
         try:
-            url=base_url+"?api_key="+key+"&query="+utils.web.urlquote(text)
+            url=base_url+"?api_key="+apiKey+"&query="+utils.web.urlquote(text)
             res=utils.web.htmlToText(utils.web.getUrl(url))
             res=json.loads(res)
         except:
+            # need better error handling here
             irc.reply("Error: I couldn't get a response from TMDb.")
             return
+        
         if len(res['results'])==0:
             irc.reply("Sorry, I couldn't find anything on that one.")
             return
@@ -51,18 +72,26 @@ class _Plugin(callbacks.Plugin):
         res_id = str(res['id'])
         res_adult = str(res['adult'])
         try:
-            url=base_url2+res_id+"?api_key="+key
+            url=base_url2+res_id+"?api_key="+apiKey
+            if language:
+                url=url+'&language='+language
             res2=utils.web.htmlToText(utils.web.getUrl(url))
             res2=json.loads(res2)
             res2_ov=res2['overview']
         except:
             res2_ov="unable to obtain description"
-        text = ('%s (%s) %s/10 http://www.themoviedb.org/movie/%s %s' % (res_title, res_date, res_avg, res_id, res2_ov))
+        
+        newUrl='http://www.themoviedb.org/movie/%s' % (res_id)
+        if language:
+            newUrl=self._get_redirected_url(newUrl) + '?language=' + language
+
+        text = ('%s (%s) %s/10 %s %s' % (res_title, res_date, res_avg, newUrl, res2_ov))
         text = text.replace("\n", " ")  # results may contain new lines which need to be stripped
         irc.reply(text)
-    movie = wrap(movie, ['text'])
+        
+    movie = wrap(movie, [getopts({'language':'somethingWithoutSpaces'}), 'text'])
 
-    def tv(self, irc, msg, args, text):
+    def tv(self, irc, msg, args, opts, text):
         """<show>
 
         Get information on <show> from TMDb
@@ -70,15 +99,22 @@ class _Plugin(callbacks.Plugin):
         
         try:
             f = open('%s%stmdbkey.dat' % (conf.supybot.directories.data(), os.sep), 'r')
-            key=f.read().strip()
+            apiKey=f.read().strip()
             f.close
         except:
             irc.reply('Error: missing or key.  Check tmdbkey.dat file in your data folder.')
             return
+
+        # input options
+        language=None
+        for (k, value) in opts:
+            if k == 'language':
+                language=value
+
         base_url = "https://api.themoviedb.org/3/search/tv"
         base_url2 = "https://api.themoviedb.org/3/tv/"
         try:
-            url=base_url+"?api_key="+key+"&query="+utils.web.urlquote(text)
+            url=base_url+"?api_key="+apiKey+"&query="+utils.web.urlquote(text)
             res=utils.web.htmlToText(utils.web.getUrl(url))
             res=json.loads(res)
         except:
@@ -93,19 +129,25 @@ class _Plugin(callbacks.Plugin):
         res_avg = str(res['vote_average'])
         res_id = str(res['id'])
         try:
-            url=base_url2+res_id+"?api_key="+key
+            url=base_url2+res_id+"?api_key="+apiKey
+            if language:
+                url=url+'&language='+language
             res2=utils.web.htmlToText(utils.web.getUrl(url))
             res2=json.loads(res2)
             res2_ov=res2['overview']
         except:
             res2_ov="unable to obtain description"
-        text = ('%s (%s) %s/10 http://www.themoviedb.org/tv/%s %s' % (res_title, res_date, res_avg, res_id, res2_ov))
+
+        newUrl='http://www.themoviedb.org/tv/%s' % (res_id)
+        if language:
+            newUrl=self._get_redirected_url(newUrl) + '?language=' + language
+
+        text = ('%s (%s) %s/10 %s %s' % (res_title, res_date, res_avg, newUrl, res2_ov))
         text = text.replace("\n", " ")  # results may contain new lines which need to be stripped
         irc.reply(text)
-    tv = wrap(tv, ['text'])
+    tv = wrap(tv, [getopts({'language':'somethingWithoutSpaces'}), 'text'])
 
-
-    def person(self, irc, msg, args, text):
+    def person(self, irc, msg, args, opts, text):
         """<person>
 
         Get information on <person> from TMDb
@@ -113,15 +155,22 @@ class _Plugin(callbacks.Plugin):
         
         try:
             f = open('%s%stmdbkey.dat' % (conf.supybot.directories.data(), os.sep), 'r')
-            key=f.read().strip()
+            apiKey=f.read().strip()
             f.close
         except:
             irc.reply('Error: missing or key.  Check tmdbkey.dat file in your data folder.')
             return
+
+        # input options
+        language=None
+        for (k, value) in opts:
+            if k == 'language':
+                language=value
+
         base_url = "https://api.themoviedb.org/3/search/person"
         base_url2 = "https://api.themoviedb.org/3/person/"
         try:
-            url=base_url+"?api_key="+key+"&query="+utils.web.urlquote(text)
+            url=base_url+"?api_key="+apiKey+"&query="+utils.web.urlquote(text)
             res=utils.web.htmlToText(utils.web.getUrl(url))
             res=json.loads(res)
         except:
@@ -134,7 +183,9 @@ class _Plugin(callbacks.Plugin):
         res_name = ircutils.bold(res['name'])
         res_id = str(res['id'])
         try:
-            url=base_url2+res_id+"?api_key="+key
+            url=base_url2+res_id+"?api_key="+apiKey
+            if language:
+                url=url+'&language='+language
             res2=utils.web.htmlToText(utils.web.getUrl(url))
             res2=json.loads(res2)
             res2_bio=res2['biography']
@@ -159,7 +210,12 @@ class _Plugin(callbacks.Plugin):
             text = ('%s (%s- )' % (text, res2_bd))
         if res2_bp:
             text = ('%s %s' % (text, res2_bp))
-        text = ('%s http://www.themoviedb.org/person/%s' % (text, res_id))
+
+        newUrl='http://www.themoviedb.org/person/%s' % (res_id)
+        if language:
+            newUrl=self._get_redirected_url(newUrl) + '?language=' + language
+
+        text = ('%s %s' % (text, newUrl))
         if res2_hp:
             text = ('%s Homepage %s' % (text, res2_hp))
         if res2_bio:
@@ -168,7 +224,7 @@ class _Plugin(callbacks.Plugin):
             text = ('%s no biography available' % text)
         text = text.replace("\n", " ")  # results may contain new lines which need to be stripped
         irc.reply(text)
-    person = wrap(person, ['text'])
+    person = wrap(person, [getopts({'language':'somethingWithoutSpaces'}), 'text'])
 
 _Plugin.__name__=PluginName
 Class = _Plugin
