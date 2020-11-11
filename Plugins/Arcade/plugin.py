47225ff4a4710460598c608386fd0ed403aa26ac
SpiderDave <ifallelsefailsusefire@gmail.com>
2020-11-11 18:09:53 -0500

    Removing Klov and replacing it with Arcade

--- Plugins/Arcade/plugin.py
+++ Plugins/Arcade/plugin.py
@@ -34,7 +34,7 @@ class _Plugin(callbacks.Plugin):
         parser.close()
         return ''.join(result)
 
-    def klov(self, irc, msg, args, text):
+    def arcadeinfo(self, irc, msg, args, text):
         """<game>
         
         video game information on <game> from http://www.arcade-museum.com
@@ -54,7 +54,29 @@ class _Plugin(callbacks.Plugin):
                 irc.reply(s)
             else:
                 irc.reply("Error")
-    klov = wrap(klov, ['text'])
+    arcadeinfo = wrap(arcadeinfo, ['text'])
+
+    def pinballinfo(self, irc, msg, args, text):
+        """<game>
+        
+        pinball information on <game> from http://www.arcade-museum.com
+        """
+        text = utils.web.urlquote(text)
+        url = format('http://www.arcade-museum.com/results.php?boolean=AND&type=Pinball&q=%s', text)
+        html = utils.web.getUrl(url)
+        notfound = re.search(b'Number of Search Results: 0', html, re.I | re.S)
+        if notfound:
+            irc.reply('Not found')
+            self.log.info(url)
+        else:
+            m = re.search(b'<TH>Name</TH>.*?<A HREF="game_detail.php\?game_id=(.*?)">(.*?)</A></TD><TD>(.*?)</TD><TD>(.*?)</TD>', html, re.I | re.S)
+            if m:
+                s = ircutils.bold(utils.str.normalizeWhitespace(m.group(2).decode('ASCII'))) + " // "+utils.str.normalizeWhitespace(m.group(3).decode('ASCII')) + " // " +utils.str.normalizeWhitespace(m.group(4).decode('ASCII')) + " // " + "http://www.arcade-museum.com/game_detail.php?game_id=" +utils.str.normalizeWhitespace(m.group(1).decode('ASCII'))
+                s = self._strip_tags(s)
+                irc.reply(s)
+            else:
+                irc.reply("Error")
+    pinballinfo = wrap(pinballinfo, ['text'])
 
 
 _Plugin.__name__=PluginName
--- Plugins/Klov/README.txt
+++
@@ -1,6 +0,0 @@
-Search for information on arcade games via the International Arcade Museum at http://www.arcade-museum.com
-
-[18:28] <SpiderDave> .klov superman
-[18:28] <Lyla> Superman // Taito // 1988 // http://www.arcade-museum.com/game_detail.php?game_id=9977
-
-Note: May not work in Python 2 anymore.
