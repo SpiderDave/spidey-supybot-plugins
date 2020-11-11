f41570b651d206becc7d163c35333314e8b00612
SpiderDave <ifallelsefailsusefire@gmail.com>
2020-11-11 17:17:31 -0500

    python 2 to 3 fixes

--- Plugins/Isitdown/__init__.py
+++ Plugins/Isitdown/__init__.py
@@ -14,7 +14,7 @@ import supybot.world as world
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
-__version__ = ""
+__version__ = "0"
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
 __author__ = supybot.Author('SpiderDave', 'SpiderDave','no e-mail')
@@ -26,14 +26,15 @@ __contributors__ = {}
 # This is a url where the most recent plugin package can be downloaded.
 __url__ = 'https://github.com/SpiderDave/spidey-supybot-plugins'
 
-import config
-import plugin
+from . import config
+from . import plugin
+from imp import reload
 reload(plugin) # In case we're being reloaded.
 # Add more reloads here if you add third-party modules and want them to be
 # reloaded when this plugin is reloaded.  Don't forget to import them as well!
 
 if world.testing:
-    import test
+    from . import test
 
 Class = plugin.Class
 configure = config.configure
